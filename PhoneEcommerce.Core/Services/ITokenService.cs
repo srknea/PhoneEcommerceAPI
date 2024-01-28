46297994ea6db3dfed933ef14bfe346615dfe875
Serkan IŞIK <dehnas79@gmail.com>
2024-01-28 13:43:13 +0300

    Add ITokenService

---
+++ PhoneEcommerce.Core/Services/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using PhoneEcommerce.Core.Configuration;
+using PhoneEcommerce.Core.DTOs;
+using PhoneEcommerce.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneEcommerce.Core.Services
+{
+    internal interface ITokenService
+    {
+        TokenDto CreateToken(AppUser userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
