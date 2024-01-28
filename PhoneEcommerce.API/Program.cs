4ffa45bf01cb7bc3d24f3867fc398df61953f2c0
Serkan IŞIK <dehnas79@gmail.com>
2024-01-28 13:24:31 +0300

    GenericRepository and UnitOfWork

--- PhoneEcommerce.API/Program.cs
+++ PhoneEcommerce.API/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PhoneEcommerce.Core.Model;
+using PhoneEcommerce.Core.Repositories;
+using PhoneEcommerce.Core.UnitOfWork;
 using PhoneEcommerce.Repository;
+using PhoneEcommerce.Repository.Repositories;
+using PhoneEcommerce.Repository.UnitOfWork;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
